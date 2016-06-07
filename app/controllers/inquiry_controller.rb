c64a55355ad58a706f5e41685ea01400651475c2
Masashi Komiya <mkomiya1330@gmail.com>
2016-06-07 14:58:56 +0000

    ログイン機能の実装

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -1,7 +1,7 @@
 class InquiryController < ApplicationController
     def index
         if params[:back]
-            @inquiry = Inquiry.new(inquiry)
+            @inquiry = Inquiry.new(inquiry_params)
             render :action => 'index'
         else
             @inquiry = Inquiry.new
@@ -10,7 +10,7 @@ class InquiryController < ApplicationController
     end
 
     def confirm
-        @inquiry = Inquiry.new(inquiry)
+        @inquiry = Inquiry.new(inquiry_params)
         if @inquiry.valid?
             render :action => 'confirm'
         else
@@ -19,12 +19,12 @@ class InquiryController < ApplicationController
     end
 
     def thanks
-        @inquiry = Inquiry.create(inquiry)
+        @inquiry = Inquiry.create(inquiry_params)
         render :action => 'thanks'
     end
 
     private
-        def inquiry
+        def inquiry_params
             params.require(:inquiry) .permit(:name, :email, :phone, :message)
         end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp 
+ActionMailer::Base.smtp_settings =
+{ 
+:user_name => "調べたSENDGRID_USERNAME", 
+:password => "調べたSENDGRID_PASSWORD", 
+:domain => "heroku.com", 
+:address => "smtp.sendgrid.net", 
+:port => 2525, 
+:authentication => :plain, 
+:enable_starttls_auto => true 
+}
