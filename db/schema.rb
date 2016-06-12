f2571b7b2c6e9a3bdc8c29c42579a8f8e35d2959
Masashi Komiya <mkomiya1330@gmail.com>
2016-06-12 07:19:51 +0000

    投稿者の設定

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160610074314) do
+ActiveRecord::Schema.define(version: 20160612070319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160610074314) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "inquiries", force: :cascade do |t|
