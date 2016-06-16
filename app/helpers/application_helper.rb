09b9a06a204e07a94f48db00b83f4892733b68cd
Masashi Komiya <mkomiya1330@gmail.com>
2016-06-16 04:43:02 +0000

    imagemagicとcarrierwaveをインストール

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    
+    def profile_img(user)
+        if user.provider == 'facebook'
+            img_url = "https://graph.facebook.com/#{user.uid}/picture?type=small"
+        elsif user.provider == 'twitter'
+            img_url = "http://furyu.nazo.cc/twicon/#{user.name}"
+        else
+            gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+            img_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        end
+        image_tag(img_url, alt: user.name)
+    end
 end
