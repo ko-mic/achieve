1b7da4de30e566862c7dabe8d198dc253d720b97
Masashi Komiya <mkomiya1330@gmail.com>
2016-06-17 11:20:07 +0000

    アセットパイプラインを削除→再作成した

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
-//= require turbolinks
 //= require_tree .
-
 //= require bootstrap-sprockets
-//= require bootstrap
-//= require bootstrap/dropdown
-
+//= require turbolinks
