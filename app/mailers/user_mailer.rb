d5a10d61f5ce535c615fbfc106ef2f068d8f28a0
Masashi Komiya <mkomiya1330@gmail.com>
2016-06-27 09:16:25 +0000

    悪さをしていたと思われる使わないUsermailer.rbファイルの中身を削除

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,25 +1,2 @@
 class UserMailer < ApplicationMailer
-    
-    # -*- coding: utf-8 -*-
-    require 'mail'
-
-    Mail.defaults do
-        delivery_method :smtp, {
-            :address => "smtp.gmail.com",
-            :port => 587,
-            :domain => 'example.com',
-            :user_name => 'achieverockycrag@gmail.com',
-            :password => 'n9ikKL6L',
-            :authentication => 'plain',
-            :enable_starttls_auto => true
-            
-        }
-    end
-    
-    m = Mail.new do
-        from "achieverockycrag@gmail.com"
-        to @inquiry.email
-        subject "お問い合わせ受領メール"
-        body @inquiry.message
-    end
 end
