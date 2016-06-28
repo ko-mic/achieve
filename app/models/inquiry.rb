d0363bc5fcba4f28ba85dadca67f8ffcc4ebd688
Masashi Komiya <mkomiya1330@gmail.com>
2016-06-28 05:45:42 +0000

    問い合わせの電話番号を数値のみにバリデーション追加した

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,5 +2,5 @@ class Inquiry < ActiveRecord::Base
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }#, uniqueness: { case_sensitive: false }
-    validates :phone, presence: true, length: { in: 10..13 }
+    validates :phone, presence: true, length: { in: 10..13 }, numericality: true
 end
