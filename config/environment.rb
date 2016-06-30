d13edc78ac070fac7f3c0a1b72d6cf2ce3e3b59b
ko-mic <mkomiya1330@gmail.com>
2016-06-30 03:25:21 +0000

    SENDGRIDの情報を整理

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,8 @@ Rails.application.initialize!
 ActionMailer::Base.delivery_method = :smtp 
 ActionMailer::Base.smtp_settings =
 { 
-:user_name => "app51414444@heroku.com", 
-:password => "cjp38oj48804", 
+:user_name => ENV['SENDGRID_USERNAME'],
+:password => ENV['SENDGRID_PASSWORD'],
 :domain => "heroku.com", 
 :address => "smtp.sendgrid.net", 
 :port => 2525, 
